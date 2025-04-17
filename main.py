5264ce7a0584d0e931093483d3c80f6126817276
localmanplaysgames <efitton5491@gmail.com>
2025-04-17 20:23:26 +0100

    book print working

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book_filepath):
+    with open(book_filepath) as book:
+        file_contents = book.read()
+    return file_contents
+
+def main():
+    getbook = get_book_text("books/frankenstein.txt")
+    print(getbook)
+
+main()

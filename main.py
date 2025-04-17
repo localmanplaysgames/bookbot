659daa13784da4227196871628c0da2891dbc3f8
localmanplaysgames <efitton5491@gmail.com>
2025-04-17 20:34:08 +0100

    notation added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# open the book specified by the filepath and retrieve the text within it
 def get_book_text(book_filepath):
     with open(book_filepath) as book:
         book_text = book.read()
     return book_text
 
+# get the text from get_book_text and count the number of words in it
 def count_book_words(book_filepath):
     book_text = get_book_text(book_filepath)
     word_count = len(book_text.split())
